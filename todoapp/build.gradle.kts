8cd1ff68609abb7a7f1ca04a28345587676311a8
Leonard Weaver <leonard.weaver45@gmail.com>
2026-06-12 14:30:06 -0500

    Uploading Enviroment setup into main branch so everyone can start with the same settings

---
+++ todoapp/build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+	java
+	id("org.springframework.boot") version "4.1.0"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.teamtetra"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-webmvc")
+	compileOnly("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+	testCompileOnly("org.projectlombok:lombok")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	testAnnotationProcessor("org.projectlombok:lombok")
+	implementation("org.xerial:sqlite-jdbc:3.53.2.0")
+	implementation("org.hibernate.orm:hibernate-community-dialects:8.0.0.Alpha1")
+	implementation("io.jsonwebtoken:jjwt-api:0.13.0")
+	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.13.0")
+	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.13.0")
+	testImplementation("com.h2database:h2:2.4.240")
+
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
