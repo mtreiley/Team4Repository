6a711b6d79eb2f4a7219b3466f221e71d3eea450
Leonard Weaver <leonard.weaver45@gmail.com>
2026-06-16 14:16:41 -0500

    Added user service and registration failure exception

---
+++ todoapp/src/main/java/com/teamtetra/todoapp/service/UserService.java
@@ -0,0 +1,60 @@
+package com.teamtetra.todoapp.service;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.teamtetra.todoapp.entity.User;
+import com.teamtetra.todoapp.exception.RegistrationFailure;
+import com.teamtetra.todoapp.repo.UserRepo;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    
+    private final UserRepo userRepo;
+
+    public void registerUser(User user){
+        //Check username
+        if(isNull(user.getUsername())) throw new RegistrationFailure("Username should not be empty");
+        if(!isCorrectLength(user.getUsername())) throw new RegistrationFailure("Username should be between 5 and 15 characters");
+        if(isUnique(user.getUsername())) throw new RegistrationFailure("Username must be unique");
+        //check password
+        if(isNull(user.getPassword())) throw new RegistrationFailure("Password should not be empty");
+        if(!isCorrectLength(user.getPassword())) throw new RegistrationFailure("Password should be between 5 and 15 characters");
+        if(!hasRequiredCharacters(user.getPassword())) throw new RegistrationFailure("Password requires all special characters");
+
+
+
+        userRepo.save(user);
+    }
+
+    public boolean isCorrectLength(String credential){
+        return (credential.length() >=5 && credential.length() <= 15);
+    }
+
+    public boolean isNull(String credential){
+        return credential == null;
+    }
+
+    public boolean hasRequiredCharacters(String credential){
+        boolean hasLowerCase = false;
+        boolean hasUpperCase = false;
+        boolean hasDigit = false;
+
+        for(char c : credential.toCharArray()){
+            if(Character.isLowerCase(c)) hasLowerCase = true;
+            if(Character.isUpperCase(c)) hasUpperCase = true;
+            if(Character.isDigit(c)) hasDigit = true;
+            if(hasLowerCase && hasUpperCase && hasDigit) return true;
+        }
+        return false;
+    }
+
+    public boolean isUnique(String credential){
+        Optional<User> userOptional =  userRepo.findByUsername(credential);
+        return userOptional.isPresent();
+    }
+}
